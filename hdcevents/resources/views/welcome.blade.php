86a2a4a91fb6160e6b7462edc0e4034036a46ce1
Gabriel-hp <gdias6187@gmail.com>
2024-08-12 15:14:22 -0400

    laravel

---
+++ hdcevents/resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/css/style.css">
+        <script src="/js/scripts.js"></script>
+        <title>Laravel</title>
+
+        </style>
+    </head>
+    <body>
+       <h1>algum titulo</h1>
+       
+    </body>
+</html>
